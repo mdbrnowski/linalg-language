dff5eb61a4104d5d427f0f5286b6d66eab15dc48
Michał Dobranowski <mdbrnowski@gmail.com>
2024-10-23 15:18:57 +0200

    Modify main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+#!/usr/bin/env python3
 from antlr4 import *
 from generated.MyLexer import MyLexer
+from generated.MyParser import MyParser
 
 
-with open('example.txt', encoding='utf-8') as f:
-    string = f.read()
+def pretty_lexer(file: str):
+    with open(file, encoding='utf-8') as f:
+        string = f.read()
 
-lexer = MyLexer(InputStream(string))
+    lexer = MyLexer(InputStream(string))
 
-for token in lexer.getAllTokens():
-    token_name = lexer.ruleNames[token.type - 1]
-    print(f'({token.line}): {token_name}({token.text})')
+    for token in lexer.getAllTokens():
+        token_name = lexer.ruleNames[token.type - 1]
+        print(f'({token.line}): {token_name}({token.text})')
+
+def pretty_parser(file: str):
+    with open(file, encoding='utf-8') as f:
+        string = f.read()
+
+    lexer = MyLexer(InputStream(string))
+    stream = CommonTokenStream(lexer)
+    parser = MyParser(stream)
+
+    tree = parser.program()
+    # print(tree.toStringTree(recog=parser))
+
+if __name__ == '__main__':
+    # pretty_lexer('example.txt')
+    pretty_parser('example3.txt')
