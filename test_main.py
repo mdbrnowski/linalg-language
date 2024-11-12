bee4eeeea6b3a4be1e3f162c94c29ee30602f16b
Michał Dobranowski <mdbrnowski@gmail.com>
2024-11-12 10:31:27 +0100

    Add parser tests

--- test_main.py
+++ test_main.py
@@ -4,6 +4,29 @@ from main import app
 runner = CliRunner()
 
 
+def test_parser_basic():
+    result = runner.invoke(app, ["parse", "tests/parser/input_1.txt"])
+    assert result.exit_code == 0
+    assert result.stdout == ""
+
+
+def test_parser_loops():
+    result = runner.invoke(app, ["parse", "tests/parser/input_2.txt"])
+    assert result.exit_code == 0
+    assert result.stdout == ""
+
+
+def test_parser_error():
+    result = runner.invoke(app, ["parse", "tests/parser/invalid_input_1.txt"])
+    assert result.exit_code == 0
+    assert "line 1" in result.stdout
+    assert "line 3" in result.stdout
+    assert "line 7" in result.stdout
+    assert "line 10" in result.stdout
+    assert "line 12" in result.stdout
+    assert result.stdout.count("line") == 5
+
+
 def test_ast():
     result = runner.invoke(app, ["ast", "tests/ast/input_1.txt"])
     assert result.exit_code == 0
